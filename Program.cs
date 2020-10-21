c7ba3a724b39b78348b2f2314358d75c77eb6811
Santhosh V <santhosh.v6426@gmail.com>
2020-10-22 02:47:01 +0530

    [Santhosh] Add . Added Program to Check the Equality of Lines.

--- Program.cs
+++ Program.cs
@@ -25,19 +25,43 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program.");
-            ///to get the value of points from the user.
-            Console.WriteLine("Enter the Value of x1 of first point : ");
-            float x1 =Single.Parse( Console.ReadLine());
-            Console.WriteLine("Enter the Value of y1 of first point : ");
-            float y1 =Single.Parse( Console.ReadLine());
-            Console.WriteLine("Enter the Value of x2 of Second point : ");
-            float x2 = Single.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the Value of y2 of second point : ");
-            float y2 = Single.Parse(Console.ReadLine());
-            ///to calculate the length of a line
-            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-            ///Display the length of a line.
-            Console.WriteLine("The length of the line is : "+distance);
+            double distance = 0;
+            double distance2 = 0;
+            for (int i = 1; i <= 2; i++)
+            {
+                Console.WriteLine("Inputs for Line "+i);
+                ///to get the value of points from the user.
+                Console.WriteLine("Enter the Value of x1 of first point : ");
+                float x1 = Single.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Value of y1 of first point : ");
+                float y1 = Single.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Value of x2 of Second point : ");
+                float x2 = Single.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Value of y2 of second point : ");
+                float y2 = Single.Parse(Console.ReadLine());
+                if (i == 1)
+                {
+                    ///to calculate the length of a line 1.
+                    distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+                    ///Display the length of a line 1.
+                    Console.WriteLine("The length of the line "+i+" is : " + distance);
+                }
+                if (i == 2)
+                {
+                    ///to calculate the length of a line 2
+                    distance2 = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+                    ///Display the length of a line 2.
+                    Console.WriteLine("The length of the line "+i+" is : " + distance2);
+                }
+            }
+            ///To check the equality of the lines
+            if (distance == distance2)
+            {
+                Console.WriteLine("The Lines are Equal");
+            }
+            else {
+                Console.WriteLine("The lines are not Equal!");
+            }
         }
     }
 }
